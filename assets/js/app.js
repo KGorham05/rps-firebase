7b275d81712c2651be43b0d582e9b33117ef63b1
Kgor <kevin.gorham@gmail.com>
2019-03-14 18:13:05 -0700

    some base wireframing of elements

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,24 @@
-$("h1").html("<h1>Hello friends!</h1>")
+
+
+// Rock Paper Scissors Game
+
+// When first visiting page, each user must login using firebase authentication or create an account 
+
+// on log in, show find game screen
+    // create a button to host a match
+        // on click change the color/text of that button
+        // update the game list
+        // don't allow someone who has a game created to join an open game 
+    // create a table of current matches waiting for players
+        // add a join game button
+            // on click, execute modal that says "2 players connected, game will start in 5...4..."
+            // when modal expires, change game state to "started" or "choice phase"
+// on game started state/screen - 
+    // Display "You have 10 seconds to lock in your choice!" in message box
+    // display Rock Paper Scissors images - 
+        // on click change a state to "x selected"
+            // change color of pick by changing image source, use js to increase the size of the selected element 
+    // display chat window as sticky footer
+    
+        
+
---
+++ assets/js/config.js
@@ -0,0 +1,13 @@
+var config = {
+    apiKey: "AIzaSyArKyVdY7jZHk9pHp1V1rnP4oiUa4sKuiQ",
+    authDomain: "rps-multiplayer-a8385.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-a8385.firebaseio.com",
+    projectId: "rps-multiplayer-a8385",
+    storageBucket: "",
+    messagingSenderId: "183768295978"
+};
+
+firebase.initializeApp(config);
+var database = firebase.database();
+
+
